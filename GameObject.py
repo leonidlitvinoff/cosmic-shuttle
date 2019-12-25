3f14742d205e9e690814d26e586cc2d940be87f0
stf2003.kem@gmail.com <bpKz!UH$uDRZzoTiCnaQ66cetx7a*QALGG%9wgcnM0H%b4S^mPnAd6#MiAT!3k0T&v7zsN@N>
2019-12-25 21:04:12 +0700

    Changed abstract class: GameObject

--- GameObject.py
+++ GameObject.py
@@ -1,20 +1,24 @@
 import pygame
 
+FPS = 60
+
 
 class GameObject(pygame.sprite.Sprite):
-    def __init__(self, position, size, sound_path=None):
+
+    """Абстрактный класс. Предстовляет из себя простой игровой обьект"""
+
+    def __init__(self, position, sound_path=None):
         super().__init__()
 
         self.position = position
-        self.size = size
         self.sound = pygame.mixer.music.load(sound_path)
 
     def play_sond(self):
         if self.sound:
             self.sound.play()
 
-    def get_size(self):
-        return self.size
-
     def get_position(self):
         return self.position
+
+    def update(self):
+        pass
