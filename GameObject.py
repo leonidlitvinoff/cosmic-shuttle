a148f93a4d57149d41a0a8d88b4f941b387e6782
stf2003.kem@gmail.com <bpKz!UH$uDRZzoTiCnaQ66cetx7a*QALGG%9wgcnM0H%b4S^mPnAd6#MiAT!3k0T&v7zsN@N>
2019-12-26 10:26:30 +0700

    Changed class: EmptyObject Changed class: EmptyPoint Added class: GameObject

--- GameObject.py
+++ GameObject.py
@@ -7,13 +7,13 @@ class EmptyPoint(pygame.sprite.Sprite):
     def __init__(self, position, path_sound=None):
         super().__init__()
 
-        self.size = pygame.Rect(position, (1, 1))
+        self.rect = pygame.Rect(position, (1, 1))
         if path_sound:
             self.sound = pygame.mixer.sound(path_sound)
         self.sound = None
 
     def get_position(self):
-        return self.size.x, self.size.y
+        return self.rect.x, self.rect.y
 
     def play_sound(self):
         if self.sound:
@@ -27,11 +27,15 @@ class EmptyObject(EmptyPoint):
     def __init__(self, position, size, path_sound=None):
         super().__init__(position, path_sound)
 
-        self.size.size = size
+        self.rect.size = size
 
     def get_size(self):
-        return self.size.size
+        return self.rect.size
 
     def get_rect(self):
-        return self.size
+        return self.rect
 
+
+class GameObject(EmptyObject):
+    def __init__(self, position, size, path_sound=None):
+        super().__init__(position, size, path_sound)
