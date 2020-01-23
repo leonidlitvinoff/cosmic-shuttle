f6d2c0823a3408909128da75f0768b04c7edc7be
stf2003.kem@gmail.com <bpKz!UH$uDRZzoTiCnaQ66cetx7a*QALGG%9wgcnM0H%b4S^mPnAd6#MiAT!3k0T&v7zsN@N>
2020-01-23 19:48:19 +0700

    Global Fix file: GameObject(Changed All classes)

--- GameObjects.py
+++ GameObjects.py
@@ -5,44 +5,285 @@ FPS = 60
 
 
 class EmptyObject(pygame.sprite.Sprite):
-    def __init__(self, position, size=(1, 1), path_sound=None):
+
+    """Пустой обьект"""
+
+    def __init__(self, position, size=(1, 1), path_sound=None, tag='None', name='None'):
+        """Иницилизация Обьекта"""
+        # Иницилизация родителя
         super().__init__()
 
+        # Иницилизация
         self.rect = pygame.Rect(position, size)
+        self.name = name
+        self.tag = tag
+
+        # Если есть музыка, сохраняем её
         if path_sound:
             self.sound = pygame.mixer.sound(path_sound)
-        self.sound = None
+        else:
+            self.sound = None
+
+    def get_info(self):
+        """Возращяет название обьекта"""
+
+        return self.name
+
+    def get_tag(self):
+        """Возвращяет тег обьекта"""
+
+        return self.tag
 
     def get_position(self):
+        """Возращяет позицию обьекта"""
+
         return self.rect.x, self.rect.y
 
     def play_sound(self):
+        """Проигрывает звук обьекта"""
+
         if self.sound:
             self.sound.play()
 
     def get_size(self):
+        """Возращяет размер обьекта (w, h)"""
+
         return self.rect.size
 
     def get_rect(self):
+        """Возращяет класс (pygame.Rect) с задаными параметрами"""
+
         return self.rect
 
+    def set_radius(self, radius):
+        """Устанавливает радиус обьекту"""
+
+        self.r = radius
+
+    def set_mask(self, mask):
+        """Устонваливает маску обьекту, по стандарту из self.image"""
+
+        self.mask = mask
+
     def shift(self, position):
+        """Принудиьтельно сдвинуть обьект, на (x, y) пикселей"""
+
         self.rect.move_ip(*position)
 
     def update(self, *arg, **kwargs):
+        """Обновление обьекта"""
+        # Обновление родительских функций
         super().update(arg)
 
+        # Проигрывание музыки
         self.play_sound()
+
+
+class VisibleObject(EmptyObject):
+
+    """Видимый обьект"""
+
+    def __init__(self, position, path_image, path_sound=None, animation=None, tag='None', name='None'):
+        """Иницилизаця обьекта"""
+
+        # Загрузка изображения
+        self.image = pygame.image.load(path_image)
+
+        # Если включена анимация
+        if animation:
+            self.frames = []
+            self.cur_frame = animation[2] if len(animation) == 3 else 1
+
+            self.speed_anim = FPS // animation[3] if len(animation) == 4 else 0
+            self.counter_anim = 0
+
+            self.col = animation[0]
+            self.row = animation[1]
+
+            w, h = self.image.get_width() // self.col, self.image.get_height() // self.row
+
+            for j in range(self.row):
+                for i in range(self.col):
+                    frame_location = (w * i, h * j)
+                    self.frames.append(self.image.subsurface(
+                        pygame.Rect(frame_location, (w, h))))
+            self.image = self.frames[self.cur_frame]
+        self.animation = animation
+
+        # Иницилизация родителя
+        super().__init__(position, self.image.get_size(), path_sound, tag, name)
+
+    def set_mask(self, mask=None):
+        """Установить маску обьету"""
+
+        # Если не передали маску по стандарту получаем её с изображения
+        if mask is None:
+            self.mask = pygame.mask.from_surface(self.image)
+        else:
+            super().set_mask(mask)
+
+    def get_surface(self):
+        """Возвращяет изображение обьекта"""
+
+        return self.image
+
+    def disabled_alpha(self):
+        """Отключения альфаканала"""
+
+        self.image = self.image.convert()
+
+    def play_animation(self):
+        """Проигрывание анимации"""
+
+        # Если анимация включена
+        if self.animation:
+            # Счётчик текушей анимации
+            self.counter_anim += 1
+
+            # Если скрость анимации прошла меняем картинку
+            if self.counter_anim >= self.speed_anim:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.counter_anim = 0
+
+    def update(self, *arg, **kwargs):
+        """Обновления обьетка"""
+
+        # Обновление родительских функций
+        super().update()
+
+        # Проигрывание анимации
+        self.play_animation()
+
+
+class VisibleMovingObject(VisibleObject):
+
+    """Видимый обьект способный двигаться"""
+
+    def __init__(self, position, path_image, path_sound=None, speed_move=1,
+                 animation=None, tag='None', name='None'):
+        """Иницилизация обьекта"""
+
+        # Иницилизация родителя
+        super().__init__(position, path_image, path_sound, animation, tag, name)
+
+        # Сохранения движения по осям (x, y) в зависимости от типа
+        if type(speed_move) in (int, float):
+            speed_move = speed_move / FPS
+            self.speed_move = (speed_move, speed_move)
+        else:
+            self.speed_move = (speed_move[0] / FPS,
+                               speed_move[1] / FPS)
+
+        # Счётчик скорости
+        self.counter_speed = [0, 0]
+
+    def edit_speed_move(self, new_speed_move):
+        """Изменение скорости обьекта"""
+
+        self.speed_move = new_speed_move
+
+    def move_x(self, revers=False):
+        """Движение по оси x"""
+
+        # Ведём счёт
+        self.counter_speed[0] += self.speed_move[0]
+
+        if self.counter_speed[0] >= 1:
+
+            # берём целое число и движемся по оси x
+            roun = int(self.counter_speed[0])
+            if revers:
+                self.rect.move_ip(-roun, 0)
+            else:
+                self.rect.move_ip(roun, 0)
+
+            # отнимаем от счётчика пройденый путь
+            self.counter_speed[0] -= roun
+
+    def move_y(self, revers=False):
+        """Движение по оси y"""
+
+        # Ведём счёт
+        self.counter_speed[1] += self.speed_move[1]
+
+        if self.counter_speed[1] >= 1:
+
+            # берём целое число и движемся по оси y
+            roun = int(self.counter_speed[1])
+            if revers:
+                self.rect.move_ip(0, -roun)
+            else:
+                self.rect.move_ip(0, roun)
+
+            # отнимаем от счётчика пройденый путь
+            self.counter_speed[1] -= roun
+
+    def get_speed_move(self):
+        """Возращяет скорость передвижения обьекта"""
+
+        return self.speed_move
+
+
+class GameObject(VisibleMovingObject):
+
+    """Полнеценный игровой обьект со всемы функциями"""
+
+    def __init__(self, position, path_image, path_sound=None, speed_move=0,
+                 animation=None, time_life=None, hp=None, tag='None', name='None'):
+        """Иницилизация"""
+
+        # Иницилизация родителя
+        super().__init__(position, path_image, path_sound,
+                         speed_move, animation, tag, name)
+
+        # Добавление новых возможностей
+        self.time_life = time_life
+        self.hp = hp
+
+    def hit(self, damage):
+        """Получение урона"""
+
+        # Если существует hp отнимаем от него дамаг до смерти
+        if self.hp:
+            self.hp -= damage
+            if self.hp <= 0:
+                self.kill()
+            return True
+        else:
+            return False
+
+    def update_time_life(self):
+        """Обновление время жизни"""
+
+        # Если есть жизнь отнимаём после чего смерть
+        if self.time_life is not None:
+            if self.time_life == 0:
+                self.time_life = None
+                self.kill()
+            else:
+                self.time_life -= 1
+
+    def update(self, *arg, **kwargs):
+        """Обновление обьекта"""
+
+        # Обновление родительских функций
+        super().update()
+
+        # Обновление время жизни
+        self.update_time_life()
 
 
 class Camera(EmptyObject):
     def __init__(self, current_position=(0, 0), size=(0, 0), path_sound=None,
-                 flags=None):
-        if flags is None:
-            flags = pygame.FULLSCREEN
-        self.screen = pygame.display.set_mode(size, flags)
+                 flags=0, depth=0, display=0, tag='None', name='None'):
+        self.screen = pygame.display.set_mode(size, flags, depth, display)
+
+        """Иницилизация камеры"""
+
+        # Иницилизация родителей
         super().__init__(current_position, self.screen.get_rect().size,
-                         path_sound)
+                         path_sound, tag, name)
 
     def get_screen(self):
         return self.screen
@@ -51,9 +292,9 @@ class Camera(EmptyObject):
 class MovingCamera(Camera):
     def __init__(self, traffic_restriction=(None, None),
                  current_position=(0, 0), size=(0, 0), path_sound=None,
-                 flags=None, speed_move=1, max_speed_increase=2,
-                 distance_start_move=5):
-        super().__init__(current_position, size, path_sound, flags)
+                 flags=0, depth=0, display=0, speed_move=1, max_speed_increase=2,
+                 distance_start_move=5, tag='None', name='None'):
+        super().__init__(current_position, size, path_sound, flags, depth, display, tag, name)
 
         if type(speed_move) == int:
             self.speed_move = (speed_move, speed_move)
@@ -67,12 +308,12 @@ class MovingCamera(Camera):
 
         self.traf_x, self.traf_y = traffic_restriction
 
+
     def update(self, all_sprite, *arg, **kwargs):
         super().update(arg, kwargs)
 
         mouse_pos = pygame.mouse.get_pos()
         w, h = self.get_size()
-        all_sprite = all_sprite
 
         x, y = 0, 0
 
@@ -107,152 +348,36 @@ class MovingCamera(Camera):
         self.dy += y
 
 
-class TransparentObject(EmptyObject):
-    def __init__(self, position, size, path_sound=None):
-        super().__init__(position, size, path_sound)
+class TargetCamera(Camera):
+    def __init__(self, target, size=(0, 0), path_sound=None,
+                 flags=0, depth=0, display=0, tag='None', name='None'):
+        """Иницилизация"""
 
-    def set_collision_radius(self, radius):
-        self.r = radius
+        # Иницилизация родителя
+        super().__init__(target.get_position(), size, path_sound,
+                 flags, depth, display, tag, name)
 
-    def set_collision_mask(self, mask):
-        self.mask = mask
+        # Сохранение цели
+        self.target = target
 
+    def move(self, all_sprite, shift_cord):
+        """Движение камеры"""
 
-class VisibleObject(TransparentObject):
-    def __init__(self, position, path_image,
-                 path_sound=None, animation=None):
+        # Принемаем скорость персонажа и двигаем всех относительно персонажа
+        speed_move = self.target.get_speed_move()
+        for sprite in all_sprite.sprites():
+            sprite.shift((speed_move[0] * shift_cord[0],
+                          speed_move[1] * shift_cord[1]))
 
-        self.image = pygame.image.load(path_image)
+    def update(self, all_sprite, shift_cord, *arg, **kwargs):
+        """Обновление обьекта"""
 
-        if animation:
-            self.frames = []
-            self.cur_frame = animation[2] if len(animation) == 3 else 1
+        # Обновление родителя
+        super().update(arg)
 
-            self.speed_anim = FPS // animation[3] if len(animation) == 4 else 0
-            self.counter_anim = 0
-
-            self.col = animation[0]
-            self.row = animation[1]
-
-            w, h = self.image.get_width() // self.col, self.image.get_height() // self.row
-
-            for j in range(self.row):
-                for i in range(self.col):
-                    frame_location = (w * i, h * j)
-                    self.frames.append(self.image.subsurface(
-                        pygame.Rect(frame_location, (w, h))))
-            self.image = self.frames[self.cur_frame]
-        self.animation = animation
-
-        super().__init__(position, self.image.get_size(), path_sound)
-
-    def set_collision_mask(self, mask=None):
-        if mask is None:
-            self.mask = pygame.mask.from_surface(self.image)
-        else:
-            super().set_collision_mask(mask)
-
-    def disabled_alpha(self):
-        self.image = self.image.convert()
-
-    def update(self, *arg, **kwargs):
-        super().update()
-
-        if self.animation:
-            self.counter_anim += 1
-
-            if self.counter_anim >= self.speed_anim:
-                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-                self.image = self.frames[self.cur_frame]
-                self.counter_anim = 0
-
-
-class Mouse(VisibleObject):
-    def __init__(self, position, path_image,
-                 path_sound=None, animation=None):
-        super().__init__(position, path_image,
-                         path_sound, animation)
-
-
-class VisibleMovingObject(VisibleObject):
-    def __init__(self, position, path_image,
-                 path_sound=None, speed_move=1, animation=None):
-        super().__init__(position, path_image, path_sound,
-                         animation)
-        if type(speed_move) in (int, float):
-            speed_move = speed_move / FPS
-            self.speed_move = (speed_move, speed_move)
-        else:
-            self.speed_move = (speed_move[0] / FPS,
-                               speed_move[1] / FPS)
-
-        self.counter_speed = [0, 0]
-
-    def move_x(self, strs=None):
-        self.counter_speed[0] += self.speed_move[0]
-        if self.counter_speed[0] >= 1:
-            roun = round(self.counter_speed[0])
-            if strs:
-                self.rect.move_ip(-roun, 0)
-            else:
-                self.rect.move_ip(roun, 0)
-            self.counter_speed[0] -= roun
-
-    def move_y(self, strs=None):
-        self.counter_speed[1] += self.speed_move[1]
-        if self.counter_speed[1] >= 1:
-            roun = round(self.counter_speed[1])
-            if strs:
-                self.rect.move_ip(0, -roun)
-            else:
-                self.rect.move_ip(0, roun)
-            self.counter_speed[1] -= roun
-
-
-class GameObject(VisibleMovingObject):
-    def __init__(self, position, path_image,
-                 path_sound=None, speed_move=0, animation=None, time_life=None,
-                 hp=None):
-        super().__init__(position, path_image, path_sound,
-                         speed_move, animation)
-
-        self.time_life = time_life
-        self.hp = hp
-
-    def hit(self, damage):
-        self.hp -= damage
-        if self.hp <= 0:
-            self.kill()
-
-    def update(self, *arg, **kwargs):
-        super().update()
-
-        if self.time_life is not None:
-            if self.time_life == 0:
-                self.kill()
-            else:
-                self.time_life -= 1
-
+        # Обновление движения
+        self.move(all_sprite, shift_cord)
 
 # --------------------------------------
 # Раздел модификаторов к базовым классам
 # --------------------------------------
-
-
-class Planet(GameObject):
-    def __init__(self, position, path_image,
-                 path_sound=None, speed_move=0, animation=None, time_life=None,
-                 hp=None, point_degradation=None, money=0, population=2):
-        super().__init__(position, path_image,
-                         path_sound, speed_move, animation, time_life,
-                         hp)
-        self.money = money
-        self.population = population
-        self.point_degradation = point_degradation
-
-    def update(self, *arg, **kwargs):
-        super().update(*arg, **kwargs)
-
-        if self.population < self.point_degradation:
-            self.population += round(self.population * random.random())
-            self.money += round(self.population ** 0.5)
--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ COLOR_WHITE = (255, 255, 255)
 DIFFICULTY = ['EASY']
 FPS = 60.0
 MENU_BACKGROUND_COLOR = (228, 55, 36)
-WINDOW_SIZE = (1920, 1080)
+WINDOW_SIZE = (1600, 900)
 
 clock = None
 main_menu = None
@@ -45,17 +45,13 @@ def play_function(difficulty, font, test=False):
     all_sprite = pygame.sprite.Group()
 
     background = GameObjects.GameObject((0, 0), path_from_background)
-    background.set_collision_mask()
+    background.set_mask()
     background.disabled_alpha()
     all_sprite.add(background)
 
-    planet = GameObjects.Planet((0, 0), path_from_planet, point_degradation=100)
-    all_sprite.add(planet)
-
     person = GameObjects.GameObject((100, 100), path_from_person, hp=100, speed_move=(300, 600))
-    all_sprite.add(person)
 
-    camera = GameObjects.MovingCamera(traffic_restriction=background.get_size(), speed_move=(2, 3), max_speed_increase=10, distance_start_move=10)
+    camera = GameObjects.TargetCamera(person, flags=(pygame.FULLSCREEN))
     screen = camera.get_screen()
 
     clock = pygame.time.Clock()
@@ -70,15 +66,16 @@ def play_function(difficulty, font, test=False):
                 if event.key == pygame.K_SPACE:
                     person.hit(10)
 
+        x, y = 0, 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
-            person.move_x()
+            x -= 1
         if keys[pygame.K_a]:
-            person.move_x(1)
+            x += 1
         if keys[pygame.K_w]:
-            person.move_y(1)
+            y += 1
         if keys[pygame.K_s]:
-            person.move_y()
+            y -= 1
 
         ab = pygame.sprite.collide_mask(background, person)
         if ab:
@@ -86,9 +83,10 @@ def play_function(difficulty, font, test=False):
 
         all_sprite.update()
         all_sprite.draw(screen)
+        screen.blit(person.get_surface(), person.get_rect())
 
         clock.tick(FPS)
-        camera.update(all_sprite)
+        camera.update(all_sprite, (x, y))
         pygame.display.flip()
 
 
